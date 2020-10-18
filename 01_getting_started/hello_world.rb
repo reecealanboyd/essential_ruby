05a3b80952913960e00030427a795f5de55c7326
Reece Boyd <reecealanboyd@users.noreply.github.com>
2020-10-18 10:33:13 -0500

    chapter 1: getting started

---
+++ 01_getting_started/hello_world.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+puts 'Hello World'
+
+# notes:
+# line 1 is called an interpreter directive--or a shebang. 
+# this allows you to run the file with ./<filename> assuming
+# you've given the script executable permissions like so
+# chmod u+x <filename>
